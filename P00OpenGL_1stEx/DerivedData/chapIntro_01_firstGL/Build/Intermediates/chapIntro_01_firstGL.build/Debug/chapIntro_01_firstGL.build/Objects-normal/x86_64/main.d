92783d1db0ab7b8c6a125fc3b2adb11929fe59d7
dknife <ymkang@San302-YMKang.local>
2015-03-04 14:14:53 +0900

    firstExample
    firstExample

---
+++ P00OpenGL_1stEx/DerivedData/chapIntro_01_firstGL/Build/Intermediates/chapIntro_01_firstGL.build/Debug/chapIntro_01_firstGL.build/Objects-normal/x86_64/main.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/ymkang/myWork_iMac/Mac_3_DocsPresentation/교재개발/OGL_ExampleCode/chapIntro_01_firstGL/chapIntro_01_firstGL/main.cpp
---
+++ P00OpenGL_1stEx/chapIntro_01_firstGL/main.cpp
@@ -0,0 +1,33 @@
+#ifdef WIN32 // window
+#include <windows.h>
+#include <gl/gl.h>
+#include <gl/glut.h>
+#else // mac
+#include <OpenGL/OpenGL.h>
+#include <GLUT/GLUT.h>
+#endif
+
+void myDisplay() {
+    glClear(GL_COLOR_BUFFER_BIT);
+    glBegin(GL_POLYGON);
+    glColor3f(1.0, 0.0, 0.0);
+    glVertex3f(0.0, 0.0, 0.0);
+    glColor3f(0.0, 0.0, 1.0);
+    glVertex3f(0.5, 0.0, 0.0);
+    glColor3f(0.0, 1.0, 0.0);
+    glVertex3f(0.0, 0.5, 0.0);
+    glEnd();
+    glFlush();
+}
+
+int main (int argc, char * argv[]) {
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE|GLUT_RGBA);
+    glutInitWindowPosition(0, 0);
+    glutInitWindowSize(512, 512);
+    glutCreateWindow("A Triangle");  // ^{\it 윈도우 생성}^
+    glClearColor(1.0, 0.0, 0.0, 1.0);
+    glutDisplayFunc(myDisplay);	// ^{\it 디스플레이 콜백 등록}^
+    glutMainLoop();				// ^{\it 이벤트 루프로}^
+    return 0;
+}
